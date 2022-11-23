22d5041f90f7578e89afe77c970d396cc7bf7ab2
Michael Bivins <mike.bivins@gmail.com>
2022-11-22 19:40:38 -0800

    Body Dye Script and Color Convert Extension

---
+++ Chaos/Extensions/ColorSwap.cs
@@ -0,0 +1,90 @@
+﻿using Chaos.Common.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaos.Extensions
+{
+    public static class ColorSwap
+    {
+        public static DisplayColor ConvertToDisplayColor(this BodyColor value)
+        {
+            switch (value)
+            {
+                case BodyColor.White:
+                    return DisplayColor.White;
+
+                case BodyColor.Pale:
+                    return DisplayColor.Mustard;
+
+                case BodyColor.Brown:
+                    return DisplayColor.Brown;
+
+                case BodyColor.Green:
+                    return DisplayColor.Green;
+
+                case BodyColor.Yellow:
+                    return DisplayColor.Lemon;
+
+                case BodyColor.Tan:
+                    return DisplayColor.Tan;
+
+                case BodyColor.Grey:
+                    return DisplayColor.Grey;
+
+                case BodyColor.LightBlue:
+                    return DisplayColor.Cerulean;
+
+                case BodyColor.Orange:
+                    return DisplayColor.Orange;
+
+                case BodyColor.Purple:
+                    return DisplayColor.Default;
+
+                default:
+                    return DisplayColor.Mustard;
+            }
+        }
+
+        public static BodyColor ConvertToBodyColor(this DisplayColor value)
+        {
+            switch (value)
+            {
+                case DisplayColor.White:
+                    return BodyColor.White;
+
+                case DisplayColor.Mustard:
+                    return BodyColor.Pale;
+
+                case DisplayColor.Brown:
+                    return BodyColor.Brown;
+
+                case DisplayColor.Green:
+                    return BodyColor.Green;
+
+                case DisplayColor.Lemon:
+                    return BodyColor.Yellow;
+
+                case DisplayColor.Tan:
+                    return BodyColor.Tan;
+
+                case DisplayColor.Grey:
+                    return BodyColor.Grey;
+
+                case DisplayColor.Cerulean:
+                    return BodyColor.LightBlue;
+
+                case DisplayColor.Orange:
+                    return BodyColor.Orange;
+
+                case DisplayColor.Default:
+                    return BodyColor.Purple;
+
+                default:
+                    return BodyColor.Pale;
+            }
+        }
+    }
+}
---
+++ Chaos/Scripts/DialogScripts/BodyDyeScript.cs
@@ -0,0 +1,61 @@
+﻿using Chaos.Common.Definitions;
+using Chaos.Extensions.Common;
+using Chaos.Factories;
+using Chaos.Factories.Abstractions;
+using Chaos.Objects.Menu;
+using Chaos.Objects.Panel;
+using Chaos.Objects.World;
+using Chaos.Scripts.DialogScripts.Abstractions;
+using Chaos.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaos.Scripts.DialogScripts
+{
+    public class BodyDyeScript : DialogScriptBase
+    {
+
+        private readonly IItemFactory ItemFactory;
+
+        public BodyDyeScript(Dialog subject, IItemFactory itemFactory) : base(subject)
+        {
+            ItemFactory = itemFactory;
+        }
+
+        public override void OnDisplaying(Aisling source)
+        {
+            foreach (BodyColor color in Enum.GetValues<BodyColor>())
+            {
+                var item = ItemFactory.CreateFaux("hairDyeContainer");
+                item.DisplayName = $"{color} Body Dye";
+                item.Color = Chaos.Extensions.ColorSwap.ConvertToDisplayColor(color);
+                Subject.Items.Add(item);
+            }
+        }
+
+        public override void OnNext(Aisling source, byte? optionIndex = null)
+        {
+            if (!Subject.MenuArgs.TryGet<string>(0, out var dye))
+            {
+                Subject.Reply(source, DialogString.UnknownInput.Value);
+                return;
+            }
+            var Item = Subject.Items.FirstOrDefault(x => x.DisplayName.EqualsI(dye));
+            if (Item == null)
+            {
+                Subject.Reply(source, DialogString.UnknownInput.Value);
+                return;
+            }
+            if (!source.TryTakeGold(Item.Template.BuyCost))
+            {
+                Subject.Close(source);
+                return;
+            }
+            source.BodyColor = Chaos.Extensions.ColorSwap.ConvertToBodyColor(Item.Color);
+            source.Refresh(true);
+        }
+    }
+}
