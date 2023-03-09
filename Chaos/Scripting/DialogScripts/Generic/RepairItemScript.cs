33ca33cebeaaa5f2dcd24521c1ff0cafefd2f374
Michael Bivins <mike.bivins@gmail.com>
2023-03-08 19:06:37 -0500

    Item Repair

---
+++ Chaos/Scripting/DialogScripts/Generic/RepairItemScript.cs
@@ -0,0 +1,58 @@
+﻿using Chaos.Common.Definitions;
+using Chaos.Objects.Menu;
+using Chaos.Objects.World;
+using Chaos.Scripting.DialogScripts.Abstractions;
+using Chaos.Services.Factories.Abstractions;
+
+namespace Chaos.Scripting.DialogScripts.Generic;
+
+public class RepairItemScript : DialogScriptBase
+{
+    private int _repairCost;
+
+    public RepairItemScript(Dialog subject)
+        : base(subject)
+    {
+        
+    }
+
+    public override void OnDisplaying(Aisling source)
+    {
+        foreach (var item in source.Equipment)
+        {
+            if (item.Template.MaxDurability != null && item.CurrentDurability != null && item.CurrentDurability.Value != item.Template.MaxDurability.Value)
+            {
+                var damage = ((float)item.CurrentDurability.Value / item.Template.MaxDurability.Value);
+                var formula = item.Template.SellValue / 2.0 * (.8 * damage);
+                _repairCost = (((int)(_repairCost + formula)));
+            }
+        }
+        
+        if (_repairCost != 0) 
+            Subject.Text = $"I can repair all of your items for {_repairCost}. Would you like to continue?";
+        else
+        {
+            Subject.Text = "I cannot repair your items any further.";
+            Subject.Type = MenuOrDialogType.Normal;
+        }
+    }
+
+    public override void OnNext(Aisling source, byte? optionIndex = null)
+    {
+        if (!source.TryTakeGold(_repairCost))
+        {
+            Subject.Close(source);
+            source.SendOrangeBarMessage($"You do not have enough. You need {_repairCost} gold.");
+            return;
+        }
+        
+        foreach (var repair in source.Equipment)
+        {
+            if (repair.CurrentDurability != repair.Template.MaxDurability)
+            {
+                repair.CurrentDurability = repair.Template.MaxDurability;
+            }
+        }
+        source.SendOrangeBarMessage($"Your items have been repaired.");
+    }
+}
---
+++ Chaos/Scripting/DialogScripts/Generic/RepairSingleItemScript.cs
@@ -0,0 +1,102 @@
+using System.Diagnostics;
+using Chaos.Data;
+using Chaos.Extensions.Common;
+using Chaos.Objects.Menu;
+using Chaos.Objects.Panel;
+using Chaos.Objects.World;
+using Chaos.Scripting.DialogScripts.Abstractions;
+using Chaos.Services.Factories.Abstractions;
+using Chaos.Utilities;
+
+namespace Chaos.Scripting.DialogScripts.Generic;
+
+public class RepairSingleItemScript : DialogScriptBase
+{
+    public RepairSingleItemScript(Dialog subject,  IItemFactory itemFactory) : base(subject)
+    {
+        ItemFactory = itemFactory;
+        
+        var requestInputText = DialogString.From(() => $"Would you like to repair {Item!.DisplayName} for {RepairCost} gold?");
+
+        InputCollector = new InputCollectorBuilder()
+            .RequestOptionSelection(requestInputText, DialogString.Yes, DialogString.No)
+            .HandleInput(HandleInput)
+            .Build();
+    }
+
+    private bool HandleInput(Aisling source, Dialog dialog, int? option)
+    {
+        if (option is not 1)
+            return false;
+        
+        if (!source.TryTakeGold(RepairCost))
+        {
+            Subject.Close(source);
+            source.SendOrangeBarMessage($"You do not have enough. You need {RepairCost} gold.");
+             return false;
+        }
+        
+        Item.CurrentDurability = Item.Template.MaxDurability;
+        source.SendOrangeBarMessage($"Your item has been repaired.");
+        return true;
+    }
+
+    private Item? Item { get; set; }
+    private int RepairCost { get; set; }
+    public InputCollector InputCollector { get; }
+    
+    private readonly IItemFactory ItemFactory;
+    public override void OnDisplaying(Aisling source)
+    {
+        var inventory = source.Inventory.Where(x => x.Template.MaxDurability != null && x.CurrentDurability != null && x.CurrentDurability.Value != x.Template.MaxDurability.Value).ToList();
+        if (Subject.Slots.IsNullOrEmpty())
+            Subject.Slots = inventory.Select(x => x.Slot).ToList();
+    }
+
+    public override void OnNext(Aisling source, byte? optionIndex = null)
+    {
+        if (Item != null)
+        {
+            if (!source.TryTakeGold(RepairCost))
+            {
+                Subject.Close(source);
+                source.SendOrangeBarMessage($"You do not have enough. You need {RepairCost} gold.");
+            }
+        
+            Item.CurrentDurability = Item.Template.MaxDurability;
+            source.SendOrangeBarMessage($"Your item has been repaired.");
+            source.Inventory[Item.Slot]?.Update(TimeSpan.FromSeconds(1));
+            Subject.Close(source);
+        }
+        
+        if (Item == null)
+        {
+            if (!Subject.MenuArgs.TryGet<byte>(0, out var slot))
+            {
+                Subject.Reply(source, DialogString.UnknownInput.Value);
+
+                return;
+            }
+
+            var item = source.Inventory[slot];
+
+            if (item == null)
+            {
+                Subject.Reply(source, DialogString.UnknownInput.Value);
+
+                return;
+            }
+
+            Item = item;
+
+            if (item.CurrentDurability == null || item.Template.MaxDurability == null)
+                return;
+
+            var damage = ((float)item.CurrentDurability.Value / item.Template.MaxDurability.Value);
+            var formula = item.Template.SellValue / 2.0 * (.8 * damage);
+            RepairCost = (((int)(RepairCost + formula)));
+
+            InputCollector.Collect(source, Subject, optionIndex);
+        }
+    }
+}
