f3bb03d501fef91331a74286c383da69a673ada9
Michael Bivins <mike.bivins@gmail.com>
2023-05-23 20:38:14 -0400

    Gloop (Follow Script)

--- Chaos/Scripting/MerchantScripts/FollowScript.cs
+++ Chaos/Scripting/MerchantScripts/FollowScript.cs
@@ -15,6 +15,7 @@ public class FollowScript : ConfigurableMerchantScriptBase
     private MapInstance Map => Subject.MapInstance;
     private bool ShouldFollow => FollowTimer.IntervalElapsed;
     private readonly IClientRegistry<IWorldClient> ClientRegistry;
+    private int TotalGold { get; set; }
 
 
     /// <inheritdoc />
@@ -31,25 +32,64 @@ public class FollowScript : ConfigurableMerchantScriptBase
         base.Update(delta);
 
         FollowTimer.Update(delta);
-
+        var now = DateTime.UtcNow;
+        const double TIME_SPAN_SECONDS = 3;
+        
         if (!ShouldFollow)
             return;
         
         var substringEndIndex = Subject.Name.IndexOf("'s Gloop", StringComparison.Ordinal);
         var substring = Subject.Name.Substring(0, substringEndIndex);
-        
-        var player = ClientRegistry
-            .Select(c => c.Aisling);
 
-        foreach (var aisling in player)
+        var player = ClientRegistry.Where(x => x.Aisling.IsAlive && (x.Aisling.Name == substring))
+                                   .Select(x => x.Aisling)
+                                   .FirstOrDefault();
+        
+        if (player is not null)
         {
-            if (aisling.Name == substring)
+            if (Map != player.MapInstance)
             {
-                if (Map != aisling.MapInstance)
-                    Map.RemoveObject(Subject);
-                
-                if (aisling.DistanceFrom(Subject) > 4 && Map == aisling.MapInstance) 
-                    Subject.Pathfind(new Point(aisling.X, aisling.Y));
+                Subject.TraverseMap(player.MapInstance, player);
+                return;
+            }
+
+            var item = Subject.MapInstance.GetEntitiesWithinRange<GroundItem>(player)
+                              .FirstOrDefault(x => now - x.Creation > TimeSpan.FromSeconds(TIME_SPAN_SECONDS));
+
+            if (item is not null)
+            {
+                var itemDistance = Subject.DistanceFrom(item);
+                switch (itemDistance)
+                {
+                    case > 1 and < 13:
+                        Subject.Pathfind(item);
+                        break;
+                    case >= 13:
+                        Subject.WarpTo(item);
+                        break;
+                    case <= 1:
+                    {
+                        Map.RemoveObject(item);
+                        var amount = item.Item.Template.SellValue / 75;
+                        TotalGold += amount;
+                        player.TryGiveGold(amount);
+                        player.SendActiveMessage($"Glupe munched {amount} gold. That's {TotalGold} total!");
+                        break;
+                    }
+                }
+            }
+            if (item is null)
+            {
+                var playerDistance = player.DistanceFrom(Subject);
+                switch (playerDistance)
+                {
+                    case > 4 and < 13:
+                        Subject.Pathfind(new Point(player.X, player.Y));
+                        break;
+                    case >= 13:
+                        Subject.WarpTo(player);
+                        break;
+                }
             }
         }
     }
