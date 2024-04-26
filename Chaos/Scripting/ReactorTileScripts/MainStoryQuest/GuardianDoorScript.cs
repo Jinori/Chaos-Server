1ca544f53e71bd2f1ff2fd048daa7acbbe94647c
Whug001 <nmccoy915@gmail.com>
2024-04-26 15:57:07 -0500

    Main Story INW.
    Finished 1-71.

--- Chaos/Scripting/ReactorTileScripts/MainStoryQuest/GuardianDoorScript.cs
+++ Chaos/Scripting/ReactorTileScripts/MainStoryQuest/GuardianDoorScript.cs
@@ -7,31 +7,73 @@ using Chaos.Models.World.Abstractions;
 using Chaos.Scripting.ReactorTileScripts.Abstractions;
 using Chaos.Storage.Abstractions;
 
-namespace Chaos.Scripting.ReactorTileScripts;
+namespace Chaos.Scripting.ReactorTileScripts.MainStoryQuest;
 
 public class GuardianDoorScript : ConfigurableReactorTileScriptBase
 {
     private readonly ISimpleCache SimpleCache;
 
-    #region ScriptVars
-    protected Location Destination { get; init; } = null!;
-    #endregion
-
     /// <inheritdoc />
     public GuardianDoorScript(ReactorTile subject, ISimpleCache simpleCache)
         : base(subject)
-        => SimpleCache = simpleCache;
+    {
+        SimpleCache = simpleCache;
+    }
+
+    #region ScriptVars
+
+    protected Location Destination { get; init; } = null!;
+
+    #endregion
 
     /// <inheritdoc />
     public override void OnWalkedOn(Creature source)
     {
         var targetMap = SimpleCache.Get<MapInstance>(Destination.Map);
 
-        if (!source.Trackers.Enums.HasValue(MainStoryEnums.StartedArtifactsHunt))
+        switch (source.MapInstance.Name)
         {
-            var point = source.DirectionalOffset(source.Direction.Reverse());
-            source.WarpTo(source.Trackers.LastPosition as IPoint ?? point);
-            return;
+            case "West Woodlands 6":
+            {
+                if (!source.Trackers.Enums.HasValue(MainStoryEnums.StartedArtifact1))
+                {
+                    var point = source.DirectionalOffset(source.Direction.Reverse());
+                    source.WarpTo(source.Trackers.LastPosition as IPoint ?? point);
+                    return;
+                }
+            }
+                break;
+            case "Astrid North":
+            {
+                if (!source.Trackers.Enums.HasValue(MainStoryEnums.StartedArtifact3))
+                {
+                    var point = source.DirectionalOffset(source.Direction.Reverse());
+                    source.WarpTo(source.Trackers.LastPosition as IPoint ?? point);
+                    return;
+                }
+            }
+                break;
+
+            case "Crypt 10":
+            {
+                if (!source.Trackers.Enums.HasValue(MainStoryEnums.StartedArtifact2))
+                {
+                    var point = source.DirectionalOffset(source.Direction.Reverse());
+                    source.WarpTo(source.Trackers.LastPosition as IPoint ?? point);
+                    return;
+                }
+            }
+                break;
+            case "Karlopos Island East":
+            {
+                if (!source.Trackers.Enums.HasValue(MainStoryEnums.StartedArtifact4))
+                {
+                    var point = source.DirectionalOffset(source.Direction.Reverse());
+                    source.WarpTo(source.Trackers.LastPosition as IPoint ?? point);
+                    return;
+                }
+            }
+                break;
         }
 
         source.TraverseMap(targetMap, Destination);
