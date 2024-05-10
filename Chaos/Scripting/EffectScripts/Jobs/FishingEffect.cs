a7e213cd4870cbd141f4f2a99331cc451935c202
Whug001 <nmccoy915@gmail.com>
2024-05-10 16:48:43 -0500

    Fishing update.
    Fish good to go.

--- Chaos/Scripting/EffectScripts/Jobs/FishingEffect.cs
+++ Chaos/Scripting/EffectScripts/Jobs/FishingEffect.cs
@@ -1,7 +1,12 @@
-﻿using Chaos.Common.Utilities;
+﻿using Chaos.Common.Definitions;
+using Chaos.Common.Utilities;
+using Chaos.Formulae;
 using Chaos.Models.Data;
+using Chaos.Models.Legend;
 using Chaos.Models.World;
 using Chaos.Scripting.EffectScripts.Abstractions;
+using Chaos.Scripting.FunctionalScripts.Abstractions;
+using Chaos.Scripting.FunctionalScripts.ExperienceDistribution;
 using Chaos.Services.Factories.Abstractions;
 using Chaos.Time;
 using Chaos.Time.Abstractions;
@@ -13,13 +18,15 @@ public class FishingEffect(IItemFactory itemFactory) : ContinuousAnimationEffect
     private const int FISH_CATCH_CHANCE = 2;
     private const byte FISHING_ICON = 95;
     private const int FISH_STEAL_BAIT = 5;
+    
+    private IExperienceDistributionScript ExperienceDistributionScript { get; } = DefaultExperienceDistributionScript.Create();
 
     private static readonly List<KeyValuePair<string, decimal>> FishData =
     [
         new KeyValuePair<string, decimal>("uselessboot", 20),
-        new KeyValuePair<string, decimal>("trout", 25),
-        new KeyValuePair<string, decimal>("bass", 20),
-        new KeyValuePair<string, decimal>("perch", 15),
+        new KeyValuePair<string, decimal>("trout", 25), 
+        new KeyValuePair<string, decimal>("bass", 20), 
+        new KeyValuePair<string, decimal>("perch", 15), 
         new KeyValuePair<string, decimal>("pike", 10),
         new KeyValuePair<string, decimal>("rockfish", 8),
         new KeyValuePair<string, decimal>("lionfish", 6),
@@ -45,6 +52,17 @@ public class FishingEffect(IItemFactory itemFactory) : ContinuousAnimationEffect
         "The water claims your bait."
     ];
     
+    private static readonly Dictionary<string, double> FishExperienceMultipliers = new()
+    {
+        { "Trout", 0.006 },
+        { "Bass", 0.007 },
+        { "Perch", 0.008 },
+        { "Pike", 0.009 },
+        { "Rock Fish", 0.01 },
+        { "Lion Fish", 0.02 },
+        { "Purple Whopper", 0.03 }
+    };
+    
     private List<Point> FishingSpots = new();
     protected override TimeSpan Duration { get; set; } = TimeSpan.FromHours(1);
 
@@ -65,38 +83,73 @@ public class FishingEffect(IItemFactory itemFactory) : ContinuousAnimationEffect
                                .Select(x => new Point(x.X, x.Y))
                                .ToList();
 
-    protected override void OnIntervalElapsed()
+ protected override void OnIntervalElapsed()
+{
+    var aisling = AislingSubject!;
+    var playerLocation = new Point(Subject.X, Subject.Y);
+
+    if ((FishingSpots.Count == 0) || !FishingSpots.Contains(playerLocation) || !aisling.Inventory.HasCount("Fishing Bait", 1))
     {
-        var aisling = AislingSubject!;
-        var playerLocation = new Point(Subject.X, Subject.Y);
+        Subject.Effects.Terminate("Fishing");
+        return;
+    }
 
-        if ((FishingSpots.Count == 0) || !FishingSpots.Contains(playerLocation) || !aisling.Inventory.HasCount("Fishing Bait", 1))
-        {
-            Subject.Effects.Terminate("Fishing");
-            return;
-        }
-
-        if (IntegerRandomizer.RollChance(FISH_STEAL_BAIT))
-        {
-            var randomMessage = BaitLossMessages[Random.Shared.Next(BaitLossMessages.Count)];
-            
-            aisling.SendOrangeBarMessage(randomMessage);
-            aisling.Inventory.RemoveQuantity("Fishing Bait", 1);
-        }
+    if (IntegerRandomizer.RollChance(FISH_STEAL_BAIT))
+    {
+        var randomMessage = BaitLossMessages[Random.Shared.Next(BaitLossMessages.Count)];
         
-        if (!IntegerRandomizer.RollChance(FISH_CATCH_CHANCE))
-            return;
+        aisling.SendOrangeBarMessage(randomMessage);
+        aisling.Inventory.RemoveQuantity("Fishing Bait", 1);
+    }
+    
+    if (!IntegerRandomizer.RollChance(FISH_CATCH_CHANCE))
+        return;
 
-        var templateKey = FishData.PickRandomWeighted();
-        var fish = itemFactory.Create(templateKey);
+    var templateKey = FishData.PickRandomWeighted();
+    var fish = itemFactory.Create(templateKey);
 
-        if (aisling.TryGiveItem(ref fish))
-            return;
+    if (aisling.TryGiveItem(ref fish))
+    {
+        // Calculate experience based on fish caught and award it to the player
+        var tnl = LevelUpFormulae.Default.CalculateTnl(aisling);
+        var expGain = CalculateExperienceGain(aisling, tnl, fish.DisplayName);
 
+        ExperienceDistributionScript.GiveExp(aisling, expGain);
+        aisling.SendOrangeBarMessage($"You caught a {fish.DisplayName} and gained {expGain} experience!");
+        
+        // Remove fishing bait
+        aisling.Inventory.RemoveQuantity("Fishing Bait", 1);
+        UpdatePlayerLegend(aisling);
+    }
+    else
+    {
         aisling.SendOrangeBarMessage($"You caught a {fish.DisplayName}!");
         aisling.Inventory.RemoveQuantity("Fishing Bait", 1);
     }
+}
 
+private int CalculateExperienceGain(Aisling source, int tnl, string fishName)
+{
+    
+    if (!FishExperienceMultipliers.TryGetValue(fishName, out var multiplier))
+    {
+        source.SendActiveMessage("Something went wrong when trying to catch the fish!");
+
+        return 0;
+    }
+
+    return Convert.ToInt32(multiplier * tnl);
+}
+
+private void UpdatePlayerLegend(Aisling source) =>
+    source.Legend.AddOrAccumulate(
+        new LegendMark(
+            "Caught a fish",
+            "fish",
+            MarkIcon.Yay,
+            MarkColor.White,
+            1,
+            GameTime.Now));
     public override void OnTerminated()
     {
         var playerLocation = new Point(Subject.X, Subject.Y);
