5be92cda573b33fa8039dcdf62910bde5502bb4a
Michael Bivins <mike.bivins@gmail.com>
2024-06-01 21:28:25 -0400

    Arena Monster Scripts

---
+++ Chaos/Scripting/MonsterScripts/ArenaDeathScript.cs
@@ -0,0 +1,40 @@
+using Chaos.Common.Utilities;
+using Chaos.Definitions;
+using Chaos.Models.Data;
+using Chaos.Models.World;
+using Chaos.Scripting.MonsterScripts.Abstractions;
+
+namespace Chaos.Scripting.MonsterScripts;
+
+public class ArenaDeathScript : MonsterScriptBase
+{
+    /// <inheritdoc />
+    public ArenaDeathScript(Monster subject)
+        : base(subject) { }
+
+    private readonly Animation DeathAnimation = new()
+    {
+        SourceAnimation = 82,
+        AnimationSpeed = 100
+    };
+    
+    /// <inheritdoc />
+    public override void OnDeath()
+    {
+        if (!Map.RemoveEntity(Subject))
+            return;
+
+        Map.ShowAnimation(DeathAnimation.GetPointAnimation(Subject));
+        
+        Subject.Items.AddRange(Subject.LootTable.GenerateLoot());
+
+        var playersOnArenaMap = Map.GetEntities<Aisling>().Where(x => !x.Trackers.Enums.HasValue(ArenaHost.Host)).ToList();
+        
+        foreach (var item in Subject.Items)
+        {
+            var randomMember = playersOnArenaMap.PickRandom();
+            randomMember.GiveItemOrSendToBank(item);
+            randomMember.SendOrangeBarMessage($"You received {item.DisplayName} from {Subject.Name}");
+        }
+    }
+}
---
+++ Chaos/Scripting/MonsterScripts/ArenaMonsterScript.cs
@@ -0,0 +1,31 @@
+using Chaos.Models.World;
+using Chaos.Scripting.Abstractions;
+using Chaos.Scripting.MonsterScripts.Abstractions;
+
+namespace Chaos.Scripting.MonsterScripts;
+
+public class ArenaMonsterScript : CompositeMonsterScript
+{
+    private static readonly ICollection<string> ScriptKeys = new[]
+    {
+        GetScriptKey(typeof(DefaultBehaviorsScript)),
+        GetScriptKey(typeof(AggroTargetingScript)),
+        GetScriptKey(typeof(ContributionScript)),
+        GetScriptKey(typeof(CastingScript)),
+        GetScriptKey(typeof(AttackingScript)),
+        GetScriptKey(typeof(MoveToTargetScript)),
+        GetScriptKey(typeof(WanderingScript)),
+        GetScriptKey(typeof(ArenaDeathScript)),
+        GetScriptKey(typeof(DisplayNameScript))
+    };
+
+    /// <inheritdoc />
+    public ArenaMonsterScript(IScriptProvider scriptProvider, Monster subject)
+    {
+        if (scriptProvider.CreateScript<IMonsterScript, Monster>(ScriptKeys, subject) is not CompositeMonsterScript compositeScript)
+            throw new InvalidOperationException("Unable to create componentized script");
+
+        foreach (var script in compositeScript)
+            Add(script);
+    }
+}
