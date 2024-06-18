41912407f26da6c81703c498dcce676d9510a857
Legend504 <32112214+Legend504@users.noreply.github.com>
2024-06-17 22:48:03 -0500

    Lost woodlands boss scripts

---
+++ Chaos/Scripting/MonsterScripts/Boss/WestWoodlands/Grunk/GrunkBossScript.cs
@@ -0,0 +1,32 @@
+using Chaos.Models.World;
+using Chaos.Scripting.Abstractions;
+using Chaos.Scripting.MonsterScripts.Abstractions;
+
+namespace Chaos.Scripting.MonsterScripts.Boss.EastWoodlands.GoblinChief;
+
+public class GrunkBossScript : CompositeMonsterScript
+{
+    private static readonly ICollection<string> ScriptKeys = new[]
+    {
+        GetScriptKey(typeof(DefaultBehaviorsScript)),
+        GetScriptKey(typeof(GrunkEnrageScript)),
+        GetScriptKey(typeof(BossDefenseScript)),
+        GetScriptKey(typeof(AggroTargetingScript)),
+        GetScriptKey(typeof(ContributionScript)),
+        GetScriptKey(typeof(CastingScript)),
+        GetScriptKey(typeof(AttackingScript)),
+        GetScriptKey(typeof(WanderingScript)),
+        GetScriptKey(typeof(DeathScript)),
+        GetScriptKey(typeof(DisplayNameScript))
+    };
+
+    /// <inheritdoc />
+    public GrunkBossScript(IScriptProvider scriptProvider, Monster subject)
+    {
+        if (scriptProvider.CreateScript<IMonsterScript, Monster>(ScriptKeys, subject) is not CompositeMonsterScript compositeScript)
+            throw new InvalidOperationException("Unable to create componentized script");
+
+        foreach (var script in compositeScript)
+            Add(script);
+    }
+}
