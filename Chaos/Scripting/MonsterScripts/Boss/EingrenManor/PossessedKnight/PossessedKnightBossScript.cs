904ac4cb80ba6f53d79eb38ef47159fd2230bf90
Legend504 <32112214+Legend504@users.noreply.github.com>
2024-06-21 23:35:21 -0500

    boss script

---
+++ Chaos/Scripting/MonsterScripts/Boss/EingrenManor/PossessedKnight/PossessedKnightBossScript.cs
@@ -0,0 +1,34 @@
+using Chaos.Models.World;
+using Chaos.Scripting.Abstractions;
+using Chaos.Scripting.MonsterScripts.Abstractions;
+using Chaos.Scripting.MonsterScripts.Boss.Astrid.LostOrc;
+
+namespace Chaos.Scripting.MonsterScripts.Boss.EingrenManor.PossessedKnight;
+
+public class PossessedKnightBossScript : CompositeMonsterScript
+{
+    private static readonly ICollection<string> ScriptKeys = new[]
+    {
+        GetScriptKey(typeof(DefaultBehaviorsScript)),
+        GetScriptKey(typeof(PossessedKnightEnrageScript)),
+        GetScriptKey(typeof(AggroTargetingScript)),
+        GetScriptKey(typeof(ContributionScript)),
+        GetScriptKey(typeof(CastingScript)),
+        GetScriptKey(typeof(AttackingScript)),
+        GetScriptKey(typeof(WanderingScript)),
+        GetScriptKey(typeof(DeathScript)),
+        GetScriptKey(typeof(DisplayNameScript)),
+        GetScriptKey(typeof(BossDefenseScript)),
+        GetScriptKey(typeof(MoveToTargetScript)),
+    };
+
+    /// <inheritdoc />
+    public PossessedKnightBossScript(IScriptProvider scriptProvider, Monster subject)
+    {
+        if (scriptProvider.CreateScript<IMonsterScript, Monster>(ScriptKeys, subject) is not CompositeMonsterScript compositeScript)
+            throw new InvalidOperationException("Unable to create componentized script");
+
+        foreach (var script in compositeScript)
+            Add(script);
+    }
+}
---
+++ Chaos/Scripting/MonsterScripts/Boss/EingrenManor/PossessedKnight/PossessedKnightEnrageScript.cs
@@ -0,0 +1,77 @@
+using Chaos.Extensions.Geometry;
+using Chaos.Models.Data;
+using Chaos.Models.World;
+using Chaos.Scripting.MonsterScripts.Abstractions;
+using Chaos.Services.Factories.Abstractions;
+
+namespace Chaos.Scripting.MonsterScripts.Boss.EingrenManor.PossessedKnight;
+
+public sealed class PossessedKnightEnrageScript : MonsterScriptBase
+{
+    private readonly IMonsterFactory MonsterFactory;
+    private bool Bonus30Applied;
+    private bool Bonus50Applied;
+    private bool Bonus75Applied;
+
+    private Animation UpgradeAnimation { get; } = new()
+    {
+        AnimationSpeed = 100,
+        TargetAnimation = 189
+    };
+
+    /// <inheritdoc />
+    public PossessedKnightEnrageScript(Monster subject, IMonsterFactory monsterFactory)
+        : base(subject) =>
+        MonsterFactory = monsterFactory;
+
+    public override void Update(TimeSpan delta)
+    {
+        if (!Bonus75Applied && (Subject.StatSheet.HealthPercent <= 75))
+        {
+            Bonus75Applied = true;
+            //Give Bonuses
+            var attrib = new Attributes
+            {
+                AtkSpeedPct = 10, 
+                Dmg = 5
+            };
+            Subject.StatSheet.SetHealthPct(90);
+            Subject.StatSheet.AddBonus(attrib);
+            Subject.Animate(UpgradeAnimation);
+            
+        }
+
+        if (!Bonus50Applied && (Subject.StatSheet.HealthPercent <= 50))
+        {
+            Bonus50Applied = true;
+
+            var attrib = new Attributes
+            {
+                Ac = 15,
+                Dmg = 5,
+                MagicResistance = 5,
+                SkillDamagePct = 10,
+                Hit = 10
+            };
+
+            Subject.StatSheet.SetHealthPct(65);
+            Subject.StatSheet.AddBonus(attrib);
+            Subject.Animate(UpgradeAnimation);
+        }
+
+        if (!Bonus30Applied && (Subject.StatSheet.HealthPercent <= 30))
+        {
+            Bonus30Applied = true;
+
+            var attrib = new Attributes
+            {
+                Ac = 15,
+                AtkSpeedPct = 25
+            };
+
+            Subject.StatSheet.SetHealthPct(40);
+            Subject.StatSheet.AddBonus(attrib);
+            Subject.Animate(UpgradeAnimation);
+        }
+    }
+}
