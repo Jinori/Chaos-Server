6cbc050fe566fffd4b25bce43e5743e40c8ae952
Legend504 <32112214+Legend504@users.noreply.github.com>
2024-06-26 18:40:53 -0500

    Kas orc scripts and a new delay defense script

---
+++ Chaos/Scripting/MonsterScripts/KasmaniumMines/KasOrcs/KasOrcsScript.cs
@@ -0,0 +1,37 @@
+using Chaos.Models.World;
+using Chaos.Scripting.Abstractions;
+using Chaos.Scripting.MonsterScripts.Abstractions;
+using Chaos.Scripting.MonsterScripts.Boss;
+using Chaos.Scripting.MonsterScripts.Boss.CryptBosses.FunnyBones;
+using Chaos.Scripting.MonsterScripts.Boss.UndineFields;
+
+namespace Chaos.Scripting.MonsterScripts.KasmaniumMines.KasOrcs;
+
+public class KasOrcsScript : CompositeMonsterScript
+{
+    private static readonly ICollection<string> ScriptKeys = new[]
+    {
+        GetScriptKey(typeof(DefaultBehaviorsScript)),
+        GetScriptKey(typeof(MoveToTargetScript)),
+        GetScriptKey(typeof(KasOrcsEnrageScript)),
+        GetScriptKey(typeof(AggroTargetingScript)),
+        GetScriptKey(typeof(ContributionScript)),
+        GetScriptKey(typeof(CastingScript)),
+        GetScriptKey(typeof(AttackingScript)),
+        GetScriptKey(typeof(WanderingScript)),
+        GetScriptKey(typeof(DeathScript)),
+        GetScriptKey(typeof(DisplayNameScript)),
+        GetScriptKey(typeof(UFOrcRegenScript)),
+        GetScriptKey(typeof(DelayDefenseScript)),
+    };
+
+    /// <inheritdoc />
+    public KasOrcsScript(IScriptProvider scriptProvider, Monster subject)
+    {
+        if (scriptProvider.CreateScript<IMonsterScript, Monster>(ScriptKeys, subject) is not CompositeMonsterScript compositeScript)
+            throw new InvalidOperationException("Unable to create componentized script");
+
+        foreach (var script in compositeScript)
+            Add(script);
+    }
+}
