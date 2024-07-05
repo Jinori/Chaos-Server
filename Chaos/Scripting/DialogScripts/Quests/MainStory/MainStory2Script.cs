377885b8f78e45a1a8c130571beeda911b4861d2
Whug001 <nmccoy915@gmail.com>
2024-07-05 17:44:17 -0500

    Servant Enrage Script reverted, foraging effect reduced chance, mainstory script complete.
    Mainstory 99 quest needs to be tested.

--- Chaos/Scripting/DialogScripts/Quests/MainStory/MainStory2Script.cs
+++ Chaos/Scripting/DialogScripts/Quests/MainStory/MainStory2Script.cs
@@ -46,7 +46,7 @@ public class MainStory2Script(
                 }
                 
                 if (source.Trackers.Enums.HasValue(MainStoryEnums.FoundSummoner2) 
-                    || source.Trackers.Enums.HasValue(MainStoryEnums.SpawnedCreants) 
+                    || source.Trackers.Enums.HasValue(MainStoryEnums.SpawnedCreants)
                     || source.Trackers.Enums.HasValue(MainStoryEnums.StartedSummonerFight))
                 {
                     Subject.Reply(source, "It's great that you've found him again but this time we need to defeat him. Return to the Cthonic Remains and defeat the Summoner.");
@@ -55,18 +55,20 @@ public class MainStory2Script(
 
                 if (source.Trackers.Enums.HasValue(MainStoryEnums.CompletedServant))
                 {
+                    var vitality = source.UserStatSheet.MaximumHp + source.UserStatSheet.MaximumMp * 2;
+                    
                     if (source.Trackers.TimedEvents.HasActiveEvent("servantwait", out var cdtime))
                     {
                         Subject.Reply(source, $"We are still gathering information about what the Summoner is doing in the Cthonic Remains, please wait {cdtime.Remaining.Humanize()}.");
                     }
                     
-                    if (source.UserStatSheet.Level < 99)
+                    if (source.UserStatSheet.Level < 99 || vitality < 30000)
                     {
                         Subject.Reply(source, "We discovered what the summoner is up to, but unfortunately we don't feel comfortable sending you against it just yet. You are still slightly too weak to handle the Summoner.");
                         return;
                     }
                     
-                    Subject.Reply(source, "We have no new information for you yet.");
+                    Subject.Reply(source, "Skip", "summoner_initial6");
                 }
                   
                 if (source.Trackers.Enums.HasValue(MainStoryEnums.DefeatedServant))
@@ -118,6 +120,13 @@ public class MainStory2Script(
                 var trueartifact = ItemFactory.Create("trueelementalartifact");
                 source.GiveItemOrSendToBank(trueartifact);
                 source.SendOrangeBarMessage("Find the Summoner on the third floor of Eingren Manor.");
+                return;
+            }
+
+            case "summoner_initial12":
+            {
+                source.Trackers.Enums.Set(MainStoryEnums.SearchForSummoner2);
+                source.SendOrangeBarMessage("Goddess Miraelis's voice echoes in your head... 30,000 Vitality required.");
                 return;
             }
 
--- Chaos/Scripting/MonsterScripts/Boss/ServantBoss/ServantBossEnrageScript.cs
+++ Chaos/Scripting/MonsterScripts/Boss/ServantBoss/ServantBossEnrageScript.cs
@@ -16,15 +16,9 @@ public sealed class ServantBossEnrageScript : MonsterScriptBase
     private bool Bonus30Applied;
     private bool Bonus50Applied;
     private bool Bonus75Applied;
-    private bool Attack1;
-    private bool Attack2;
-    private bool Attack3;
-    private bool Attack4;
     private readonly IIntervalTimer SpellCastTimer;
-    private readonly IIntervalTimer WaitTimer;
     private readonly IIntervalTimer ActionTimer;
     private readonly Spell SpellToCast;
-    private readonly Spell SpellToCast1;
     private readonly Spell SpellToCast2;
     private readonly Spell SpellToCast3;
 
@@ -40,12 +34,10 @@ public sealed class ServantBossEnrageScript : MonsterScriptBase
     {
         var spellFactory1 = spellFactory;
         SpellToCast = spellFactory1.Create("MorrocSpell1");
-        SpellToCast1 = spellFactory1.Create("Havoc");
         SpellToCast2 = spellFactory1.Create("ardcradh");
         SpellToCast3 = spellFactory1.Create("MorrocSpell2");
         ActionTimer = new IntervalTimer(TimeSpan.FromMilliseconds(300), false);
         SpellCastTimer = new RandomizedIntervalTimer(TimeSpan.FromSeconds(8), 10, RandomizationType.Balanced, false);
-        WaitTimer = new IntervalTimer(TimeSpan.FromSeconds(4), false);
     }
 
     public override void Update(TimeSpan delta)
@@ -53,88 +45,32 @@ public sealed class ServantBossEnrageScript : MonsterScriptBase
         SpellCastTimer.Update(delta);
         ActionTimer.Update(delta);
 
-        if (ActionTimer.IntervalElapsed)
-        {
-
-            if (Attack1)
-            {
-                Attack1 = false;
-                Subject.TryUseSpell(SpellToCast);
-            }
-
-            if (Attack2)
-            {
-                Attack2 = false;
-                Subject.TryUseSpell(SpellToCast3);
-            }
-
-            if (Attack3)
-            {
-                Attack3 = false;
-
-                foreach (var target in Subject.MapInstance.GetEntitiesWithinRange<Aisling>(Subject, 10))
-                {
-                    if (target.IsDead)
-                        return;
-
-                    if (target.IsGodModeEnabled())
-                        return;
-
-                    Subject.TryUseSpell(SpellToCast2, target.Id);
-                }
-            }
-
-            if (Attack4)
-            {
-                WaitTimer.Update(delta);
-                
-                var ani = new Animation
-                {
-                    AnimationSpeed = 300,
-                    TargetAnimation = 576
-                };
-
-                Subject.Animate(ani);
-                if (WaitTimer.IntervalElapsed)
-                {
-                    Attack4 = false;
-                    Subject.TryUseSpell(SpellToCast1);
-                    WaitTimer.Reset();
-                }
-            }
-            ActionTimer.Reset();
-        }
-
         if (SpellCastTimer.IntervalElapsed)
         {
-            if (!Attack1 && !Attack2 && !Attack3 && !Attack4)
-            {
                 var roll = IntegerRandomizer.RollSingle(100);
 
                 switch (roll)
                 {
-                    case < 20:
+                    case < 30:
                         Subject.Say("Master is going to Cthonic Remains!");
-                        Attack1 = true;
+                        Subject.TryUseSpell(SpellToCast);
 
                         break;
-                    case < 45:
+                    case < 60:
                         Subject.Say("He will rule the world with the creants!");
-                        Attack2 = true;
-
-                        break;
-                    case < 70:
-                        Subject.Say("Nothing will stop master from summoning them!");
-                        Attack3 = true;
+                        Subject.TryUseSpell(SpellToCast3);
 
                         break;
                     case < 101:
-                        Attack4 = true;
-                        Subject.Say("You all will suffer!");
+                        Subject.Say("Nothing will stop master from summoning them!");
+
+                        foreach (var aisling in Subject.MapInstance.GetEntities<Aisling>()
+                                     .Where(x => x.WithinRange(Subject, 10) && !x.IsGodModeEnabled()))
+                        {
+                            Subject.TryUseSpell(SpellToCast2, aisling.Id);
+                        }
                         break;
                 }
-            }
-            SpellCastTimer.Reset();
         }
 
         if (!Bonus75Applied && (Subject.StatSheet.HealthPercent <= 75))
