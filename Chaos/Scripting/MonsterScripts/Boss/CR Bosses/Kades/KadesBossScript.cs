c40782862394d287457f57e89e9d0fef03585722
Legend504 <32112214+Legend504@users.noreply.github.com>
2024-07-05 20:38:21 -0500

    kades

---
+++ Chaos/Scripting/MonsterScripts/Boss/CR Bosses/Kades/KadesBossScript.cs
@@ -0,0 +1,34 @@
+using Chaos.Models.World;
+using Chaos.Scripting.Abstractions;
+using Chaos.Scripting.MonsterScripts.Abstractions;
+
+namespace Chaos.Scripting.MonsterScripts.Boss.CR_Bosses.Kades;
+
+public class KadesBossScript : CompositeMonsterScript
+{
+    private static readonly ICollection<string> ScriptKeys = new[]
+    {
+        GetScriptKey(typeof(DefaultBehaviorsScript)),
+        GetScriptKey(typeof(MoveToTargetScript)),
+        GetScriptKey(typeof(KadesEnrageScript)),
+        GetScriptKey(typeof(AggroTargetingScript)),
+        GetScriptKey(typeof(ContributionScript)),
+        GetScriptKey(typeof(CastingScript)),
+        GetScriptKey(typeof(AttackingScript)),
+        GetScriptKey(typeof(WanderingScript)),
+        GetScriptKey(typeof(DisplayNameScript)),
+        GetScriptKey(typeof(BossDefenseScript)),
+        GetScriptKey(typeof(ThisIsABossScript)),
+        GetScriptKey(typeof(DeathScript)),
+    };
+
+    /// <inheritdoc />
+    public KadesBossScript(IScriptProvider scriptProvider, Monster subject)
+    {
+        if (scriptProvider.CreateScript<IMonsterScript, Monster>(ScriptKeys, subject) is not CompositeMonsterScript compositeScript)
+            throw new InvalidOperationException("Unable to create componentized script");
+
+        foreach (var script in compositeScript)
+            Add(script);
+    }
+}
---
+++ Chaos/Scripting/MonsterScripts/Boss/CR Bosses/Kades/KadesEnrageScript.cs
@@ -0,0 +1,109 @@
+using Chaos.Extensions.Geometry;
+using Chaos.Models.Data;
+using Chaos.Models.World;
+using Chaos.Scripting.MonsterScripts.Abstractions;
+using Chaos.Services.Factories.Abstractions;
+
+namespace Chaos.Scripting.MonsterScripts.Boss.CR_Bosses.Kades;
+
+public sealed class KadesEnrageScript : MonsterScriptBase
+{
+    private readonly IMonsterFactory MonsterFactory;
+    private bool Bonus30Applied;
+    private bool Bonus50Applied;
+    private bool Bonus75Applied;
+
+    private Animation UpgradeAnimation { get; } = new()
+    {
+        AnimationSpeed = 100,
+        TargetAnimation = 189
+    };
+
+    /// <inheritdoc />
+    public KadesEnrageScript(Monster subject, IMonsterFactory monsterFactory)
+        : base(subject) =>
+        MonsterFactory = monsterFactory;
+
+    public override void Update(TimeSpan delta)
+    {
+        if (!Bonus75Applied && (Subject.StatSheet.HealthPercent <= 75))
+        {
+            Bonus75Applied = true;
+            //Give Bonuses
+            var attrib = new Attributes { AtkSpeedPct = 25 };
+            Subject.StatSheet.AddBonus(attrib);
+            Subject.Animate(UpgradeAnimation);
+            //Spawn Monsters
+            var rectangle = new Rectangle(Subject, 5, 5);
+
+            for (var i = 0; i <= 1; i++)
+            {
+                if (!rectangle.TryGetRandomPoint(x => Subject.MapInstance.IsWalkable(x, Subject.Type), out var point))
+                    continue;
+
+                var mobs1 = MonsterFactory.Create("Gale_guardian", Subject.MapInstance, point);
+                Subject.MapInstance.AddEntity(mobs1, point);
+            }
+        }
+
+        if (!Bonus50Applied && (Subject.StatSheet.HealthPercent <= 50))
+        {
+            Bonus50Applied = true;
+            
+            var attrib = new Attributes
+            {
+                Dmg = 10,
+                MagicResistance = 10,
+                SkillDamagePct = 10,
+                SpellDamagePct = 10
+            };
+            
+            Subject.StatSheet.AddBonus(attrib);
+            Subject.Animate(UpgradeAnimation);
+            
+            var rectangle = new Rectangle(Subject, 5, 5);
+            
+            for (var i = 0; i <= 1; i++)
+            {
+                if (!rectangle.TryGetRandomPoint(x => Subject.MapInstance.IsWalkable(x, Subject.Type), out var point))
+                    continue;
+
+                var mobs1 = MonsterFactory.Create("Ignis_guardian", Subject.MapInstance, point);
+                Subject.MapInstance.AddEntity(mobs1, point);
+            }
+        }
+
+        if (!Bonus30Applied && (Subject.StatSheet.HealthPercent <= 30))
+        {
+            Bonus30Applied = true;
+
+            var attrib = new Attributes
+            {
+                Str = 10,
+                Ac = 15,
+                AtkSpeedPct = 25,
+                Dmg = 10,
+                FlatSkillDamage = 5,
+                FlatSpellDamage = 5,
+                Hit = 20,
+                MagicResistance = 10
+            };
+
+            Subject.StatSheet.AddBonus(attrib);
+            Subject.Animate(UpgradeAnimation);
+            
+            var rectangle = new Rectangle(Subject, 5, 5);
+            
+            for (var i = 0; i <= 1; i++)
+            {
+                if (!rectangle.TryGetRandomPoint(x => Subject.MapInstance.IsWalkable(x, Subject.Type), out var point))
+                    continue;
+
+                var mobs1 = MonsterFactory.Create("terra_guardian", Subject.MapInstance, point);
+                var mobs2 = MonsterFactory.Create("tide_guardian", Subject.MapInstance, point);
+                Subject.MapInstance.AddEntity(mobs1, point);
+                Subject.MapInstance.AddEntity(mobs2, point);
+            }
+        }
+    }
+}
