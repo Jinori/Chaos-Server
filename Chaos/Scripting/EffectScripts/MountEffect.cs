67212c556fc262efe854f9f710bca567719af481
Michael Bivins <mike.bivins@gmail.com>
2025-01-13 17:38:55 -0500

    Update MountEffect.cs

--- Chaos/Scripting/EffectScripts/MountEffect.cs
+++ Chaos/Scripting/EffectScripts/MountEffect.cs
@@ -64,6 +64,8 @@ public class MountEffect : EffectBase, NonOverwritableEffectComponent.INonOverwr
 
             Subject.StatSheet.SubtractBonus(attributes);
 
+            AislingSubject?.Client.SendAttributes(StatUpdateType.Full);
+            
             if (!AislingSubject.Trackers.TimedEvents.HasActiveEvent("mount", out _) && !AislingSubject.IsGodModeEnabled())
                 AislingSubject.Trackers.TimedEvents.AddEvent("mount", TimeSpan.FromSeconds(5), true);
         }
