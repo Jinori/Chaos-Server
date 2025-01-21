975962d0a8d864f4bda858e186ffa3406f76db86
Legend504 <32112214+Legend504@users.noreply.github.com>
2025-01-20 19:27:04 -0600

    cow update

---
+++ Chaos/Scripting/MonsterScripts/Boss/SecretCowLevel/KingOfCows/KingOfCowsBossScript.cs
@@ -0,0 +1,34 @@
+using Chaos.Models.World;
+using Chaos.Scripting.Abstractions;
+using Chaos.Scripting.MonsterScripts.Abstractions;
+using Chaos.Scripting.MonsterScripts.Boss.CRBosses.UndeadKing;
+
+namespace Chaos.Scripting.MonsterScripts.Boss.SecretCowLevel.KingOfCows;
+
+public class KingOfCowsBossScript : CompositeMonsterScript
+{
+    private static readonly ICollection<string> ScriptKeys = new[]
+    {
+        GetScriptKey(typeof(DefaultBehaviorsScript)),
+        GetScriptKey(typeof(MoveToTargetScript)),
+        GetScriptKey(typeof(KingOfCowsEnrageScript)),
+        GetScriptKey(typeof(AggroTargetingScript)),
+        GetScriptKey(typeof(ContributionScript)),
+        GetScriptKey(typeof(CastingScript)),
+        GetScriptKey(typeof(AttackingScript)),
+        GetScriptKey(typeof(WanderingScript)),
+        GetScriptKey(typeof(DeathScript)),
+        GetScriptKey(typeof(DisplayNameScript)),
+        GetScriptKey(typeof(ThisIsABossScript))
+    };
+
+    /// <inheritdoc />
+    public KingOfCowsBossScript(IScriptProvider scriptProvider, Monster subject)
+    {
+        if (scriptProvider.CreateScript<IMonsterScript, Monster>(ScriptKeys, subject) is not CompositeMonsterScript compositeScript)
+            throw new InvalidOperationException("Unable to create componentized script");
+
+        foreach (var script in compositeScript)
+            Add(script);
+    }
+}
---
+++ Chaos/Scripting/MonsterScripts/Boss/SecretCowLevel/KingOfCows/KingOfCowsEnrageScript.cs
@@ -0,0 +1,109 @@
+using Chaos.Extensions.Geometry;
+using Chaos.Models.Data;
+using Chaos.Models.World;
+using Chaos.Scripting.MonsterScripts.Abstractions;
+using Chaos.Services.Factories.Abstractions;
+
+namespace Chaos.Scripting.MonsterScripts.Boss.SecretCowLevel.KingOfCows;
+
+public sealed class KingOfCowsEnrageScript : MonsterScriptBase
+{
+    private readonly IMonsterFactory MonsterFactory;
+    private bool Bonus30Applied;
+    private bool Bonus50Applied;
+    private bool Bonus75Applied;
+
+    private Animation UpgradeAnimation { get; } = new()
+    {
+        AnimationSpeed = 100,
+        TargetAnimation = 189
+    };
+
+    /// <inheritdoc />
+    public KingOfCowsEnrageScript(Monster subject, IMonsterFactory monsterFactory)
+        : base(subject) =>
+        MonsterFactory = monsterFactory;
+
+    public override void Update(TimeSpan delta)
+    {
+        
+        if (!Bonus75Applied && (Subject.StatSheet.HealthPercent <= 75))
+        {
+            Bonus75Applied = true;
+
+            var attrib = new Attributes
+            {
+                Dmg = 10,
+                MagicResistance = 15,
+                SkillDamagePct = 10,
+                SpellDamagePct = 10
+            };
+            var rectangle = new Rectangle(Subject, 5, 5);
+
+            for (var i = 0; i <= 3; i++)
+            {
+                if (!rectangle.TryGetRandomPoint(x => Subject.MapInstance.IsWalkable(x, Subject.Type), out var point))
+                    continue;
+
+                var mobs = MonsterFactory.Create("hell_bovine1", Subject.MapInstance, point);
+                Subject.MapInstance.AddEntity(mobs, point);
+            }
+
+            Subject.StatSheet.AddBonus(attrib);
+            Subject.Animate(UpgradeAnimation);
+        }
+        
+        if (!Bonus50Applied && (Subject.StatSheet.HealthPercent <= 50))
+        {
+            Bonus50Applied = true;
+            //Give Bonuses
+            var attrib = new Attributes
+            {
+                AtkSpeedPct = 15
+            };
+            Subject.StatSheet.AddBonus(attrib);
+            Subject.Animate(UpgradeAnimation);
+            //Spawn Monsters
+            var rectangle = new Rectangle(Subject, 5, 5);
+
+            for (var i = 0; i <= 5; i++)
+            {
+                if (!rectangle.TryGetRandomPoint(x => Subject.MapInstance.IsWalkable(x, Subject.Type), out var point))
+                    continue;
+
+                var mobs = MonsterFactory.Create("hell_bovine1", Subject.MapInstance, point);
+                Subject.MapInstance.AddEntity(mobs, point);
+            }
+        }
+
+        if (!Bonus30Applied && (Subject.StatSheet.HealthPercent <= 30))
+        {
+            Bonus30Applied = true;
+
+            var attrib = new Attributes
+            {
+                Str = 5,
+                Ac = 25,
+                AtkSpeedPct = 15,
+                Dmg = 5,
+                FlatSkillDamage = 150,
+                FlatSpellDamage = 150,
+                Hit = 25,
+                MagicResistance = 10
+            };
+            var rectangle = new Rectangle(Subject, 5, 5);
+
+            for (var i = 0; i <= 7; i++)
+            {
+                if (!rectangle.TryGetRandomPoint(x => Subject.MapInstance.IsWalkable(x, Subject.Type), out var point))
+                    continue;
+
+                var mobs = MonsterFactory.Create("hell_bovine1", Subject.MapInstance, point);
+                Subject.MapInstance.AddEntity(mobs, point);
+            }
+
+            Subject.StatSheet.AddBonus(attrib);
+            Subject.Animate(UpgradeAnimation);
+        }
+    }
+}
