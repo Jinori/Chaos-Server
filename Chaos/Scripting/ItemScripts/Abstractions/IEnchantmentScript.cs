fe96e200e7a730200bb089e12c6a36f05f1c2090
sichii <despereaux@live.com>
2025-03-17 12:12:01 -0400

    enchantmentScript changes

--- Chaos/Scripting/ItemScripts/Abstractions/IEnchantmentScript.cs
+++ Chaos/Scripting/ItemScripts/Abstractions/IEnchantmentScript.cs
@@ -1,4 +1,7 @@
+using Chaos.Extensions.Common;
 using Chaos.MetaData.ItemMetaData;
+using Chaos.Models.Data;
+using Chaos.Models.Panel;
 using Chaos.Models.Templates;
 
 namespace Chaos.Scripting.ItemScripts.Abstractions;
@@ -6,4 +9,31 @@ namespace Chaos.Scripting.ItemScripts.Abstractions;
 public interface IEnchantmentScript
 {
     static abstract IEnumerable<ItemMetaNode> Mutate(ItemMetaNode node, ItemTemplate template);
+}
+
+public interface IPrefixEnchantmentScript : IEnchantmentScript
+{
+    static abstract Attributes Modifiers { get; }
+    static abstract string PrefixStr { get; }
+    
+    static void ApplyPrefix<T>(Item item) where T: IPrefixEnchantmentScript
+    {
+        item.Prefix = T.PrefixStr;
+        item.Modifiers.Add(T.Modifiers);
+    }
+    
+    static void RemovePrefix<T>(Item item) where T: IPrefixEnchantmentScript
+    {
+        item.Prefix = null;
+        item.Modifiers.Subtract(T.Modifiers);
+    }
+    
+    static IEnumerable<ItemMetaNode> Mutate<T>(ItemMetaNode node, ItemTemplate template) where T: IPrefixEnchantmentScript
+    {
+        if (!node.Name.StartsWithI(T.PrefixStr))
+            yield return node with
+            {
+                Name = $"{T.PrefixStr} {node.Name}"
+            };
+    }
 }
--- Chaos/Scripting/ItemScripts/Enchantments/AiryPrefixScript.cs
+++ Chaos/Scripting/ItemScripts/Enchantments/AiryPrefixScript.cs
@@ -1,4 +1,3 @@
-using Chaos.Extensions.Common;
 using Chaos.MetaData.ItemMetaData;
 using Chaos.Models.Data;
 using Chaos.Models.Panel;
@@ -7,26 +6,22 @@ using Chaos.Scripting.ItemScripts.Abstractions;
 
 namespace Chaos.Scripting.ItemScripts.Enchantments;
 
-public sealed class AiryPrefixScript : ItemScriptBase, IEnchantmentScript
+public sealed class AiryPrefixScript : ItemScriptBase, IPrefixEnchantmentScript
 {
     /// <inheritdoc />
     public AiryPrefixScript(Item subject)
-        : base(subject)
-    {
-        Subject.Prefix = "Airy";
-
-        var attributes = new Attributes
-        {
-            Hit = 3
-        };
-
-        subject.Modifiers.Add(attributes);
-    }
+        : base(subject) => IPrefixEnchantmentScript.ApplyPrefix<AiryPrefixScript>(subject);
 
     /// <inheritdoc />
     public static IEnumerable<ItemMetaNode> Mutate(ItemMetaNode node, ItemTemplate template)
+        => IPrefixEnchantmentScript.Mutate<AiryPrefixScript>(node, template);
+
+    /// <inheritdoc />
+    public static Attributes Modifiers { get; } = new()
     {
-        if (!node.Name.StartsWithI("Airy"))
-            yield return node with { Name = $"Airy {node.Name}" };
-    }
+        Hit = 3
+    };
+
+    /// <inheritdoc />
+    public static string PrefixStr => "Airy";
 }
