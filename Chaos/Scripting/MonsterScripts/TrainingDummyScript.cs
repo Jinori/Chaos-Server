8bf47bfda7533d81f54a7ae2801b0f1af3b92040
Michael Bivins <mike.bivins@gmail.com>
2025-03-27 14:09:28 -0400

    Update TrainingDummyScript.cs

--- Chaos/Scripting/MonsterScripts/TrainingDummyScript.cs
+++ Chaos/Scripting/MonsterScripts/TrainingDummyScript.cs
@@ -1,26 +1,28 @@
 using Chaos.Models.World;
 using Chaos.Models.World.Abstractions;
 using Chaos.Scripting.MonsterScripts.Abstractions;
+using Chaos.Time;
+using Chaos.Time.Abstractions;
 
 namespace Chaos.Scripting.MonsterScripts;
 
 public class TrainingDummyScript(Monster subject) : MonsterScriptBase(subject)
 {
-    public override void OnAttacked(Creature attacker, int damage)
+    public readonly IIntervalTimer DamageTimer = new IntervalTimer(TimeSpan.FromSeconds(1));
+
+    /// <inheritdoc />
+    public override void Update(TimeSpan delta)
     {
-        if (Subject.Effects.Contains("pramh"))
-            Subject.Effects.Dispel("pramh");
+        DamageTimer.Update(delta);
 
-        if (Subject.Effects.Contains("beag pramh"))
-            Subject.Effects.Dispel("beag pramh");
+        if (!DamageTimer.IntervalElapsed)
+            return;
 
-        if (Subject.Effects.Contains("Wolf Fang Fist"))
-            Subject.Effects.Dispel("Wolf Fang Fist");
+        var damageTaken = (int)(Subject.StatSheet.EffectiveMaximumHp - Subject.StatSheet.CurrentHp);
 
-        if (Subject.Effects.Contains("Amnesia"))
-            Subject.Effects.Dispel("Amensia");
-
-        if (attacker is Aisling aisling)
-            Subject.Say($"{aisling.Name} did {damage} damage.");
+        if (damageTaken > 0)
+            Subject.Say($"{damageTaken:N0} damage.");
+        
+        Subject.StatSheet.SetHealthPct(100);
     }
 }
