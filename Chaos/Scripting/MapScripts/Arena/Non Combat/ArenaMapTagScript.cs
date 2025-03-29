375e71bea531a2e8061ae39a18cf4b2a01ded589
Whug001 <nmccoy915@gmail.com>
2025-03-29 01:40:06 -0500

    killer instinct removed from arena, new arenamaptag, fixes to study creature/sense, added show hp to prevent heal dispel.

---
+++ Chaos/Scripting/MapScripts/Arena/Non Combat/ArenaMapTagScript.cs
@@ -0,0 +1,9 @@
+using Chaos.Collections;
+using Chaos.Scripting.MapScripts.Abstractions;
+
+namespace Chaos.Scripting.MapScripts.Arena.Non_Combat;
+
+public class ArenaMapTagScript(MapInstance subject) : MapScriptBase(subject)
+{
+    //just a script to tag an arena map, does nothing.
+}
